d06993000a96339a80cea399998ab6d488056e29
Simon Achkar <>
2019-11-19 16:13:30 -0400

    Inititial designs

---
+++ src/components/Yellow.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Yellow = () => {
+  return (
+    <div className="block yellow">
+      <p>Yellow.js</p>
+      <button>Toggle Fail Notification</button>
+      <button>Toggle Success Notification</button>
+    </div>
+  );
+};
+
+export default Yellow;
