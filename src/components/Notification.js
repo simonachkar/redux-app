d257ce326d955ac80bdc84abf21391ee394ace84
Simon Achkar <simon@simonachkar.com>
2019-11-28 10:04:37 -0400

    Update Notification.js

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Notification.css'
 
-const Notification = ({ text = 'Default text', status = 'success' }) => {
+const Notification = ({ text = 'Default text', status = 'success', index }) => {
   return (
     <div className={`notification ${status}`}>
       <p>{text}</p>
